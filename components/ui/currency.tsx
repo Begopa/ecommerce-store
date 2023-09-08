dea425b46b632009c4bb692576a5c5d05b51eb81
Begopa <philip0813@naver.com>
2023-09-08 10:04:23 +0900

    feat(mainpage): featured products 가격에 통화기호가 붙도록 설정

---
+++ components/ui/currency.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+export const formatterForUSD = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+});
+
+interface CurrencyProps {
+  value?: string | number;
+}
+
+const Currency: React.FC<CurrencyProps> = ({ value }) => {
+  const [isMounted, setIsMounted] = useState();
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <div className="font-semibold">{formatterForUSD.format(Number(value))}</div>
+  );
+};
+
+export default Currency;
